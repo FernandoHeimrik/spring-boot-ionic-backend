0e2346d8f5b4dcc7b53bef07e41688889eba4328
Fernando Henrique <fernando.hnrq@gmail.com>
2019-02-09 13:31:47 -0200

    Deletando uma categoria com DELETE

--- src/main/java/com/fhzalves/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/fhzalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -6,8 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpServerErrorException;
 
+import com.fhzalves.cursomc.services.exceptions.DataIntegrityException;
 import com.fhzalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -22,4 +22,11 @@ public class ResourceExceptionHandler {
 	}
 	
 
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		 
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		 
+	}
 }
